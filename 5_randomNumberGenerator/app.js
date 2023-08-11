73ce3d170997c74a74259ed47b6fd9b49d92579e
carozum <czumbiehlfaure@hotmail.fr>
2023-08-11 09:45:00 +0200

    add changes for day 6

--- 5_randomNumberGenerator/app.js
+++ 5_randomNumberGenerator/app.js
@@ -4,25 +4,32 @@
 */
 
 // Collect an integer from the user
-const number1 = parseInt(prompt('Please enter a numer higher than 0.'));
+const number1 = parseInt(prompt('Please enter a number higher than 0.'));
 const number2 = parseInt(prompt("Please enter a different number higher than 0."));
 
 let message;
 
-if (number1 && number2 && number1!== number2){
-  let highNumber;
-  let lowNumber;
 
-  if (number1 > number2){
-    highNumber = number1;
-    lowNumber = number2;
-  } else {
-    highNumber = number2;
-    lowNumber = number1;
-  }
+/**
+ * Returns a random number between 2 numbers
+ * 
+ * @param {number} lowNumber - the lowest number value
+ * @param {number} highNumber - The highest number value
+ * @return {number} The random number value.
+ * 
+ */
 
+function randomNumber(lowNumber, highNumber){
   const randomNumber = Math.floor(Math.random() * ( highNumber - lowNumber )) + lowNumber;
-  message = `<h2>The random number between ${lowNumber} and ${highNumber} is ${randomNumber}</h2>`;
+  return randomNumber;
+}
+
+if (number1 && number2 && number1!== number2){
+  let highNumber = Math.max(number1, number2);
+  let lowNumber = Math.min(number1, number2);
+
+  const randomNum = randomNumber(lowNumber, highNumber);   
+  message = `<h2>The random number between ${lowNumber} and ${highNumber} is ${randomNum}</h2>`;
 
 } else {
   message = "You need to provide 2 different numbers. Try again ";
---
+++ 6_area/app.js
@@ -0,0 +1,26 @@
+/**
+ * [A short description of the myFunc function]
+ *
+ * @param {[param type]} param1 - [parameter description]
+ * @param {[param type]} param2 - [parameter description]
+ * @returns {[return type]} [documents the function's return value]
+ */
+
+function myFunc( param1, param2 ) {
+    // function returns a value...
+  }
+  
+
+/**
+ * Calculates and returns the area of a rectangle.
+ *
+ * @param {number} width - The width of the rectangle.
+ * @param {number} length - The length of the rectangle.
+ * @param {string} unit - The unit of measurement.
+ * @returns {string} The area of the rectangle and unit of measure.
+ */
+
+function getArea(width, length, unit) {
+  const area = width * length;
+  return `${area} ${unit}`;
+}
