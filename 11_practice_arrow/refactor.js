e2218c092a94d6b8afec7b2fbee4d9ca0015f48c
carozum <czumbiehlfaure@hotmail.fr>
2023-08-13 19:58:12 +0200

    add exercice on while loop (treehouse)

--- 11_practice_arrow/refactor.js
+++ 11_practice_arrow/refactor.js
@@ -8,3 +8,13 @@ const printToTen = () => {
 }
 
 const addUs = (num1, num2) => console.log(num1 + num2);
+
+
+function getRandomNumber(upper){
+	return Math.floor(Math.random() * upper) + 1;
+}
+let counter = 0;
+while (counter < 10){
+	counter += 1;
+	console.log(getRandomNumber(100));
+}
